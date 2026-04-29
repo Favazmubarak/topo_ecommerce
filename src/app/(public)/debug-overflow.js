64a5fe11f93716ec7058f587a934389e94cc5c46
AHAMMED FAVAZ K M <favazmubarak25@gmail.com>
2026-04-29 17:19:37 +0530

    Remove debug file and fix overflow-x for build

--- src/app/(public)/debug-overflow.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('*').forEach(el => {
-  if (el.scrollWidth > el.clientWidth) {
-    console.log('Overflowing element:', el, 'ScrollWidth:', el.scrollWidth, 'ClientWidth:', el.clientWidth);
-  }
-});
